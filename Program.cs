7b5bdc2ddf4a26c1ddc4565354fc643bc4bca7e5
vadimm461 <vadimm461@gmail.com>
2025-07-04 18:21:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {

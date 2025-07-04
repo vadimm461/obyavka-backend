a94ae2fde0684acd9740d3aa3aff8792cd026ba6
vadimm461 <vadimm461@gmail.com>
2025-07-04 18:37:14 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,8 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
